912372d4a5bd94ced8cf6095682a00e481428469
nemo1560 <signature8100@gmail.com>
2018-11-27 22:23:32 +0700

    tao vi du co ban ve gps

---
+++ app/src/main/java/com/example/nemo1/gpsexample/MainActivity.java
@@ -0,0 +1,198 @@
+package com.example.nemo1.gpsexample;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationListener;
+
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener,LocationListener, View.OnClickListener, CompoundButton.OnCheckedChangeListener {
+    private static final long UPDATE_INTERVAL = 5000;
+    private static final long FASTEST_INTERVAL = 5000;
+    private static final int REQUEST_LOCATION_PERMISSION = 100;
+
+    private GoogleApiClient googleApiClient;
+    private Location mLastLocation;
+    private LocationRequest locationRequest;
+    private boolean isAutoUpdateLocation;
+    @BindView(R.id.out_location)TextView out_location;
+    @BindView(R.id.get_location)Button get_location;
+    @BindView(R.id.switch_auto)Switch switch_auto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        get_location.setOnClickListener(this);
+        switch_auto.setOnCheckedChangeListener(this);
+        requestLocationPermissions();
+        setUpLocationClientIfNeeded();
+        buildLocationRequest();
+    }
+    //Kiem tra permission da duoc mo.
+    private void requestLocationPermissions(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                }
+                else {
+                    requestLocationPermissions();
+                }
+                break;
+        }
+    }
+    //Tao doi tuong GoogleApiClient.Builder
+    private void setUpLocationClientIfNeeded() {
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+    //Tao doi tuong LocationRequest
+    private void buildLocationRequest() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FASTEST_INTERVAL);
+    }
+    //Kiem tra GPS
+    private boolean isGpsOn() {
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return manager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    protected void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, (com.google.android.gms.location.LocationListener) this);
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, (com.google.android.gms.location.LocationListener) this);
+    }
+
+    private void updateUi() {
+        if (mLastLocation != null) {
+            out_location.setText(String.format(Locale.getDefault(), "%f, %f", mLastLocation.getLatitude(), mLastLocation.getLongitude()));
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (googleApiClient != null) {
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        if (googleApiClient != null) {
+            googleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        if (isAutoUpdateLocation) {
+            updateUi();
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        if (lastLocation != null) {
+            mLastLocation = lastLocation;
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onDestroy() {
+        if (googleApiClient != null && googleApiClient.isConnected()) {
+            stopLocationUpdates();
+            googleApiClient.disconnect();
+            googleApiClient = null;
+        }
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (isGpsOn()) {
+            Toast.makeText(MainActivity.this, "GPS is ON", Toast.LENGTH_SHORT).show();
+            updateUi();
+        } else {
+            Toast.makeText(MainActivity.this, "GPS is OFF", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (!isGpsOn()) {
+            Toast.makeText(MainActivity.this, "GPS is OFF",
+                    Toast.LENGTH_SHORT).show();
+            switch_auto.setChecked(false);
+            return;
+        }
+        isAutoUpdateLocation = isChecked;
+        if (isChecked) {
+            startLocationUpdates();
+        } else {
+            stopLocationUpdates();
+        }
+    }
+}
